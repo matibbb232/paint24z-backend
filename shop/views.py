1f337786db649e14948569233499aaf5f681e167
Mateusz Bagieński <mateuszb6a@gmail.com>
2025-01-25 12:54:33 +0100

    Add filtering products based on category

--- shop/views.py
+++ shop/views.py
@@ -1,96 +1,76 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import authentication, permissions, status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
-from rest_framework import status
-from .models import Products, Manufacturers, Categories, Store, Addresses, Orders, Test
-from .serializers import (
-    ProductsSerializer,
-    CategoriesSerializer,
-    StoreSerializer,
-    CartSerializer,
-    TestSerializer,
-    ManufacturersSerializer,
-    ProductSerializer
-)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.views import APIView
-# from rest_framework.response import Response
-from rest_framework import authentication, permissions
-# from django.contrib.auth.models import User
 
-# class Test(APIView):
-# 	# permission_classes = (permissions.IsAuthenticated,)
-# 	# authentication_classes = (SessionAuthentication,)
-# 	##
-# 	def get(self, request):
-# 		serializer = TestSerializer(request.user)
-# 		return Response({'user': serializer.data}, status=status.HTTP_200_OK)
+from .models import Addresses, Categories, Manufacturers, Orders, Products, Store, Test
+from .serializers import (
+    CartSerializer,
+    CategoriesSerializer,
+    ManufacturersSerializer,
+    ProductSerializer,
+    ProductsSerializer,
+    StoreSerializer,
+    TestSerializer,
+)
 
-
-
-@api_view(["GET"])
-def get_test(request):
-    car = Test.objects.all()
-    print(car)
-    product = get_object_or_404(Test, id=1)
-    return Response(TestSerializer(product).data)
 
 @api_view(["GET"])
-def get_products(request):
+def get_products(request: Request) -> Response:
+    """Returns list of products"""
     products = Products.objects.all()
-
     serializer = ProductsSerializer(products, many=True)
     return Response(serializer.data)
 
 
 @api_view(["GET"])
-def test_json(request):
-    # Hardcoded JSON data
-    data = {
-        "message": "Hello, this is a test JSON response!",
-        "status": "success",
-        "data": {
-            "id": 1,
-            "name": "Test Item",
-            "description": "This is a hardcoded item for demonstration purposes.",
-        },
-    }
-    return Response(data)
-
-
-@api_view(["GET"])
-def get_product(request, id):
-
-    product = get_object_or_404(Products, id=id)
-
-    serializer = ProductSerializer(product)
-
+def get_products_category(request: Request, id: int) -> Response:
+    """Returns products with given category id"""
+    products = Products.objects.filter(category_id=id)
+    serializer = ProductsSerializer(products, many=True)
     return Response(serializer.data)
 
 
 @api_view(["GET"])
-def get_categories(request):
+def get_product(request: Request, id: int) -> Response:
+    """Return products with given id"""
+    product = get_object_or_404(Products, id=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_categories(request: Request) -> Response:
+    """Returns list of categories"""
     categories = Categories.objects.all()
     print(categories)
     print(categories.first())
     serializer = CategoriesSerializer(categories, many=True)
     return Response(serializer.data)
 
+
 @api_view(["GET"])
-def get_manufacturers(request):
+def get_manufacturers(request: Request) -> Response:
+    """Returns list of manufacturers"""
     manu = Manufacturers.objects.all()
     serializer = ManufacturersSerializer(manu, many=True)
     return Response(serializer.data)
 
+
 @api_view(["GET"])
-def get_store(request):
+def get_store(request: Request) -> Response:
     store = Store.objects.all()
     serializer = StoreSerializer(store, many=True)
     return Response(serializer.data)
 
+
 @api_view(["GET"])
-def get_about(request):
+def get_about(request: Request) -> Response:
+    """Returns list with store info"""
     store = Store.objects.first()
     addresses = Addresses.objects.all()
 
@@ -101,10 +81,10 @@ def get_about(request):
         {"store": store_serializer.data, "addresses": addresses_serializer.data}
     )
 
-
+# Not implemented
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
-def get_cart(request, id):
+def get_cart(request: Request, id: int) -> Response:
     product_info = request.query_params.get("product_info", "false").lower() == "true"
     user = request.user
 
